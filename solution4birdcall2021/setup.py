9ef48733c8354a076be7d7d7617fce19ed46b831
yohei-freelance <n-yohei-1717@g.ecc.u-tokyo.ac.jp>
2021-05-15 18:22:09 +0900

    commit

---
+++ solution4birdcall2021/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='my brief experiment for birdcall2021',
+    author='yohei_freelance',
+    license='',
+)
